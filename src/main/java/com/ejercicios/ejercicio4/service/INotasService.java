7bbf216c923c404275db21009fa0a67903eba9c8
Leonard David Hernandez Parra <leonardhernandez02@gmail.com>
2024-07-11 10:55:42 -0500

    creando la interfas de notas service

---
+++ src/main/java/com/ejercicios/ejercicio4/service/INotasService.java
@@ -0,0 +1,11 @@
+package com.ejercicios.ejercicio4.service;
+
+import java.util.List;
+
+import com.ejercicios.ejercicio4.modelo.Estudiante;
+
+public interface INotasService {
+
+    public String getNotas(List<Estudiante> estudiantes);
+
+}
---
+++ src/main/java/com/ejercicios/ejercicio4/service/NotasServiceImpl.java
@@ -0,0 +1,15 @@
+package com.ejercicios.ejercicio4.service;
+
+import java.util.List;
+
+import com.ejercicios.ejercicio4.modelo.Estudiante;
+
+public class NotasServiceImpl implements INotasService{
+
+    @Override
+    public String getNotas(List<Estudiante> estudiantes) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNotas'");
+    }
+
+}
