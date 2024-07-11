3e42a0199297917f25119bac2af2ce70d2db0c6d
Leonard <Leonardhernandez02@gmail.com>
2024-07-10 19:39:55 -0500

    creando el modelo de estudiante

---
+++ src/main/java/com/ejercicios/ejercicio4/modelo/Estudiante.java
@@ -0,0 +1,61 @@
+package com.ejercicios.ejercicio4.modelo;
+
+import java.util.List;
+
+public class Estudiante {
+
+    private static int index = 0;
+    private int codigo;
+    private String nombre;
+    private String apellido;
+    private List<Double> notas;
+
+    public Estudiante(){
+        this.codigo = ++index;
+    }
+
+    public Estudiante(String nombre, String apellido){
+        this();
+        this.nombre = nombre;
+        this.apellido = apellido;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public List<Double> getNotas() {
+        return notas;
+    }
+
+    public void addNota (Double nota){
+        if (notas.size() > 4) {
+            throw new RuntimeException("No se admiten mas notas");
+        }
+        if (nota > 0.0 || nota > 5.0) {
+            throw new RuntimeException("Nota fuera de los parametros");
+        }
+        notas.add(nota);
+    }
+
+}
