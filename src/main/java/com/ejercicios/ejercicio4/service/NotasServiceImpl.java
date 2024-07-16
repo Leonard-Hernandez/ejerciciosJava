f6613aac901ce7399385614e328e268325727a1f
Leonard <Leonardhernandez02@gmail.com>
2024-07-15 21:45:55 -0500

    obteniendo las notas para procesarlas despues

--- src/main/java/com/ejercicios/ejercicio4/service/NotasServiceImpl.java
+++ src/main/java/com/ejercicios/ejercicio4/service/NotasServiceImpl.java
@@ -8,8 +8,18 @@ public class NotasServiceImpl implements INotasService{
 
     @Override
     public String getNotas(List<Estudiante> estudiantes) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getNotas'");
+        StringBuilder sb = new StringBuilder();
+
+
+        for(Estudiante estudiante: estudiantes){
+            Double nota1 = estudiante.getNotas().get(0);
+            Double nota2 = estudiante.getNotas().get(1);
+            Double nota3 = estudiante.getNotas().get(2);
+            Double nota4 = estudiante.getNotas().get(3);
+
+        }
+
+        return sb.toString();
     }
 
 }
